4f4cffe31df684d377f574e3c1f1ff6eb9fe28ea
Hamidreza <hamidrezapyrp@gmail.com>
2022-06-26 11:27:45 +0430

    create project

---
+++ src/main/java/login/pro/servlet_loginproject/ServletLoginProjectApplication.java
@@ -0,0 +1,13 @@
+package login.pro.servlet_loginproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServletLoginProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServletLoginProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/login/pro/servlet_loginproject/ServletLoginProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package login.pro.servlet_loginproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServletLoginProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
